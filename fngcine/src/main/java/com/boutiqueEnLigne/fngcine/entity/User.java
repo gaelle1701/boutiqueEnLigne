b900510bb1cbd07f3479e777f14a5ebf7f7b204f
Lafferrière <flora.lafferriere@gmail.com>
2022-01-05 12:17:23 +0100

    create user class and config db

--- fngcine/src/main/java/com/boutiqueEnLigne/fngcine/entity/User.java
+++ fngcine/src/main/java/com/boutiqueEnLigne/fngcine/entity/User.java
@@ -1,4 +1,91 @@
 package com.boutiqueEnLigne.fngcine.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="users", uniqueConstraints= {
+        @UniqueConstraint(columnNames = "email")
+})
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 20)
+    private String last_name;
+
+    @NotBlank
+    @Size(max = 20)
+    private String first_name;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+   /* @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();*/
+
+    public User() {
+    }
+
+    public User(String lastName, String firstName, String email, String password) {
+        this.last_name = lastName;
+        this.first_name = firstName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
