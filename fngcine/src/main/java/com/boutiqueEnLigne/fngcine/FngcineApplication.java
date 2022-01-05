825c458d120c75e24cd87d28ca3f1bf88f4a6705
Lafferrière <flora.lafferriere@gmail.com>
2022-01-05 10:30:41 +0100

    jout partie back

---
+++ fngcine/src/main/java/com/boutiqueEnLigne/fngcine/FngcineApplication.java
@@ -0,0 +1,13 @@
+package com.boutiqueEnLigne.fngcine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FngcineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FngcineApplication.class, args);
+	}
+
+}
---
+++ fngcine/src/test/java/com/boutiqueEnLigne/fngcine/FngcineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutiqueEnLigne.fngcine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FngcineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
