dd2e3a49ac6c9d9b3d123ff2e26c64496a8d4af5
Lafferrière <flora.lafferriere@gmail.com>
2022-01-05 15:13:40 +0100

    add product and role entities

--- fngcine/src/main/java/com/boutiqueEnLigne/fngcine/entity/Product.java
+++ fngcine/src/main/java/com/boutiqueEnLigne/fngcine/entity/Product.java
@@ -1,4 +1,23 @@
 package com.boutiqueEnLigne.fngcine.entity;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String label;
+    private String description;
+    private String url_img;
+    private String genre;
+    private float unit_price;
+    private int qty_stock;
 }
--- fngcine/src/main/java/com/boutiqueEnLigne/fngcine/entity/Role.java
+++ fngcine/src/main/java/com/boutiqueEnLigne/fngcine/entity/Role.java
@@ -1,4 +1,39 @@
 package com.boutiqueEnLigne.fngcine.entity;
 
-public enum Role {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Role name;
+
+    public Role() {
+
+    }
+
+    public Role(Role name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Role getName() {
+        return name;
+    }
+
+    public void setName(Role name) {
+        this.name = name;
+    }
 }
