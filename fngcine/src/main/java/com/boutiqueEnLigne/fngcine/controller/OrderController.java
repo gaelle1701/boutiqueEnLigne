503bee22cae70bfdaef77fad75fce40d76af124c
Lafferrière <flora.lafferriere@gmail.com>
2022-01-07 15:50:43 +0100

    ajout données bouchons, Order Logique

---
+++ fngcine/src/main/java/com/boutiqueEnLigne/fngcine/controller/OrderController.java
@@ -0,0 +1,57 @@
+package com.boutiqueEnLigne.fngcine.controller;
+
+import com.boutiqueEnLigne.fngcine.entity.Order;
+import com.boutiqueEnLigne.fngcine.entity.Product;
+import com.boutiqueEnLigne.fngcine.service.OrderService;
+import com.boutiqueEnLigne.fngcine.service.UserService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Getter
+@Setter
+@RequestMapping("api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private UserService userService;
+
+    ResponseEntity responseEntity;
+
+    @GetMapping("")
+    public ResponseEntity<List<Order>> getOrders() {
+        List<Order> orders = orderService.getOrders();
+        return ResponseEntity.ok(orders);
+    }
+
+    @GetMapping("/{id}")
+    public Order getOrder(@PathVariable("id") Long id) {
+        return orderService.getOrder(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order newOrder = orderService.createOrder(order);
+        return new ResponseEntity<>(newOrder, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Order> updateOrder(@PathVariable("id") Long id,@RequestBody Order orderToUpdate){
+        Order updatedOrder = orderService.updateOrder(orderToUpdate);
+        return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrderById(@PathVariable("id") Long id) {
+        orderService.deleteOrder(id);
+    }
+
+}
---
+++ fngcine/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO fng_db.roles (name) VALUES ('ADMIN');
+INSERT INTO fng_db.roles (name) VALUES ('USER');
+
+INSERT INTO fng_db.deliveries (label, shipping_fee) VALUES ('STANDART', 2.5);
+INSERT INTO fng_db.deliveries (label, shipping_fee) VALUES ('EXPRESS', 7.5);
+
+INSERT INTO fng_db.users (email, first_name, last_name, password, roles_id) VALUES ('johndoe@mail.com', 'Doe', 'John', 'JohnD22@', '1');
+INSERT INTO fng_db.users (email, first_name, last_name, password, roles_id) VALUES ('johndoeUser@mail.com', 'DoeUser', 'JohnUser', 'JohnD22User@', '2');
+
+INSERT INTO fng_db.products (description, genre, label, qty_stock, unit_price, url_img) VALUES ('blablabla', 'drame', 'film qui fait pleurer', '2', '8', NULL);
+INSERT INTO fng_db.products (description, genre, label, qty_stock, unit_price, url_img) VALUES ('blablabla', 'comedie', 'film qui fait rire', '10', '5', NULL);
+
+INSERT INTO fng_db.orders (delivery_address, order_date, receiver, total_price, delivery_id, user_id) VALUES ('5 rue des roses\r\n75016 Paris', '2022-01-07', 'Doe John', '250', '1', '1')
